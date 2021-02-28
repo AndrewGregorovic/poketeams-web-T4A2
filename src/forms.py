ed42344cbf790eb6b4396dda900ea0146efbbdf8
Andrew <andrew.g.93@hotmail.com>
2021-02-28 12:41:57 +1100

    Created forms.py and included the forms required for auth endpoints.

---
+++ src/forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, PasswordField, SubmitField, StringField, TextAreaField
+from wtforms.validators import DataRequired, EqualTo, Length, Email
+
+
+class SignUpForm(FlaskForm):
+    username = StringField("Username", validators=[
+        DataRequired(),
+        Length(min=3, max=30)
+    ])
+    email = StringField("Email", validators=[
+        DataRequired(),
+        Email()
+    ])
+    password = PasswordField("Password", validators=[
+        DataRequired(),
+        Length(min=6)
+    ])
+    confirm_password = PasswordField("Confirm Password", validators=[
+        DataRequired(),
+        EqualTo("password", message="Both passwords need to match.")
+    ])
+    submit = SubmitField("Sign Up")
+
+
+class LogInForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Log In")
