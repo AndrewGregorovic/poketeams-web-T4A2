63d69aa1d67d3178dc96b32ad2e1a677eb5b7c09
Andrew <andrew.g.93@hotmail.com>
2021-03-03 18:40:16 +1100

    Renamed the sequence used for the id column.

--- src/models/TeamsPokemon.py
+++ src/models/TeamsPokemon.py
@@ -4,7 +4,7 @@ from src.main import db
 class Teams_Pokemon(db.Model):
     __tablename__ = "teams_pokemon"
 
-    id = db.Column(db.Integer, db.Sequence('teams_pokemon_id'), unique=True, nullable=False)
+    id = db.Column(db.Integer, db.Sequence("teams_pokemon_id_seq"), unique=True, autoincrement=True)
     team_id = db.Column(db.Integer, db.ForeignKey("teams.id"), primary_key=True)
     team_index = db.Column(db.Integer, primary_key=True)
     pokeapi_id = db.Column(db.Integer, db.ForeignKey("pokemon.pokeapi_id"))
