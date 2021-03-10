c85116d30f2bee5b18d248cf23e1a3250bbd88c0
Andrew <andrew.g.93@hotmail.com>
2021-03-10 19:08:49 +1100

    All pokemon controller endpoints and templates are working correctly. Small change to TeamsPokemonSchema to get edit_team_slot_pokemon endpoint working.

--- src/controllers/pokemon_controller.py
+++ src/controllers/pokemon_controller.py
@@ -7,6 +7,7 @@ from src.models.Pokemon import Pokemon
 from src.models.PokemonMoves import Pokemon_Moves
 from src.models.Team import Team
 from src.models.TeamsPokemon import Teams_Pokemon
+from src.schemas.PokemonMovesSchema import pokemon_moves_schema
 from src.schemas.TeamsPokemonSchema import teams_pokemon_schema
 
 
@@ -28,11 +29,20 @@ def view_selected_pokemon(pokeapi_id):
 
 @pokemon.route("/teams/<int:team_id>/<int:team_index>", methods=["GET"])
 def view_team_pokemon(team_id, team_index):
+    form = RemovePokemonForm()
     team = Team.query.get(team_id)
     team_pokemon = Teams_Pokemon.query.get((team_id, team_index))
     pokemon_api_data = Pokemon.get_pokemon_data(team_pokemon.pokeapi_id)
+
+    move_set = Pokemon_Moves.query.filter_by(team_pokemon_id=team_pokemon.id).order_by(Pokemon_Moves.pokemon_move_index).all()
+    move_set_dict = pokemon_moves_schema.dump(move_set)
+    indices = [move.pokemon_move_index for move in move_set]
+    for i in range(4):
+        if i + 1 not in indices:
+            move_set_dict.insert(i, None)
+
     ability_data = [Pokemon.get_pokemon_ability_data(ability["ability"]["url"]) for ability in pokemon_api_data["abilities"]]
-    return render_template("pokemon_view.html", data=[pokemon_api_data, ability_data], team=team, team_id=team_id, team_index=team_index, type="team")
+    return render_template("pokemon_view.html", data=[pokemon_api_data, ability_data], form=form, moves=move_set_dict, team=team, team_id=team_id, team_index=team_index, type="team")
 
 
 @pokemon.route("/teams/<int:team_id>/<int:team_index>/select", methods=["GET"])
@@ -52,9 +62,10 @@ def get_team_pokemon_list(team_id, team_index):
 def view_selected_team_pokemon(team_id, team_index, pokeapi_id):
     team = Team.query.get(team_id)
     if current_user.id == team.owner_id:
+        form = ConfirmForm()
         pokemon_api_data = Pokemon.get_pokemon_data(pokeapi_id)
         ability_data = [Pokemon.get_pokemon_ability_data(ability["ability"]["url"]) for ability in pokemon_api_data["abilities"]]
-        return render_template("pokemon_view.html", data=[pokemon_api_data, ability_data], team_id=team_id, team_index=team_index, pokeapi_id=pokeapi_id, type="team_selected")
+        return render_template("pokemon_view.html", data=[pokemon_api_data, ability_data], form=form, team=team, team_id=team_id, team_index=team_index, pokeapi_id=pokeapi_id, type="team-selected")
 
     flash("You do not have permission to change this pokemon.")
     return redirect(url_for("pokemon.view_team_pokemon", team_id=team_id, team_index=team_index))
@@ -68,6 +79,19 @@ def edit_team_slot_pokemon(team_id, team_index, pokeapi_id):
         form = ConfirmForm()
         if form.validate_on_submit():
 
+            # Create new entry if pokemon not already in database
+            pokemon = Pokemon.query.get(pokeapi_id)
+            if not pokemon:
+                pokemon_api_data = Pokemon.get_pokemon_data(pokeapi_id)
+                pokemon = Pokemon()
+                pokemon.pokeapi_id = pokeapi_id
+                pokemon.pokemon_id = int(pokemon_api_data["species"]["url"].replace("https://pokeapi.co/api/v2/pokemon-species/", "").replace("/", ""))
+                pokemon.pokemon_name = pokemon_api_data["name"].capitalize()
+                db.session.add(pokemon)
+                db.session.commit()
+
+            # Create new entry if there is no existing team_pokemon for this team id and team index,
+            # otherwise update the existing entry
             teams_pokemon = Teams_Pokemon.query.filter_by(team_id=team_id, team_index=team_index)
             if not teams_pokemon:
                 new_team_pokemon = Teams_Pokemon()
@@ -79,13 +103,13 @@ def edit_team_slot_pokemon(team_id, team_index, pokeapi_id):
                 data = {
                     "team_id": team_id,
                     "team_index": team_index,
-                    "pokeapi_id": pokeapi_id
+                    "pokeapi_id": pokemon.pokeapi_id
                 }
 
-                teams_pokemon.update(teams_pokemon_schema.load(data))
+                teams_pokemon.update(teams_pokemon_schema.load(data, partial=True))
 
                 # Delete the saved moves of the old pokemon
-                pokemon_moves = Pokemon_Moves.query.filter_by(teams_pokemon_id=teams_pokemon[0].id)
+                pokemon_moves = Pokemon_Moves.query.filter_by(team_pokemon_id=teams_pokemon[0].id)
                 for move in pokemon_moves:
                     db.session.delete(move)
 
